18b50099c5535dab7c85248452dba1ba95592f2b
Ömer Kazak <omerkazak1441@gmail.com>
2026-01-03 05:41:12 +0300

    kategoriler tanımlandı ve 50 ürün eklendi

---
+++ data.py
@@ -0,0 +1,668 @@
+hediye_veritabanı=[
+
+
+     # Mutfak Kategorisi Başlangıcı   
+
+
+{
+
+    "id": 101,
+    "ad":"Dijital Mutfak Tartısı",
+    "kategori":["mutfak",],
+    "fiyat":300,
+    "fiyat_puanı" : 1
+},
+
+    
+{
+
+    "id": 102,
+    "ad":"Kesme Tahtası Seti",
+    "kategori":["mutfak",],
+    "fiyat":400,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 103,
+    "ad":"İsimli Mutfak Önlüğü",
+    "kategori":["mutfak",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 104,
+    "ad":"Vakumlu Saklama Kabı Seti",
+    "kategori":["mutfak",],
+    "fiyat":1000,
+    "fiyat_puanı" : 2
+},
+
+
+    
+{
+
+    "id": 105,
+    "ad":"Döküm Izgara Tava",
+    "kategori":["mutfak",],
+    "fiyat":1200,
+    "fiyat_puanı" : 2
+},
+
+
+    
+{
+
+    "id": 106,
+    "ad":"Kahve Başlangıç Seti",
+    "kategori":["mutfak",],
+    "fiyat":1400,
+    "fiyat_puanı" : 2
+},
+
+
+    
+{
+
+    "id": 107,
+    "ad":"Airfryer",
+    "kategori":["mutfak",],
+    "fiyat":3000,
+    "fiyat_puanı" : 3
+},
+
+
+    
+{
+
+    "id": 108,
+    "ad":"Kahve Makinesi",
+    "kategori":["mutfak",],
+    "fiyat":4000,
+    "fiyat_puanı" : 3
+},
+
+
+    
+{
+
+    "id": 109,
+    "ad":"Mutfak Şefi Makinesi",
+    "kategori":["mutfak",],
+    "fiyat":4000,
+    "fiyat_puanı" : 3
+},
+
+
+    #Mutfak Kategorisi Burada Bitti
+
+
+#----------------------------------------------------------
+
+
+
+    #Teknoloji Kategorisi Başlangıcı
+{
+
+    "id": 110,
+    "ad":"Airtag ",
+    "kategori":["teknoloji",],
+    "fiyat":400,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 111,
+    "ad":"Kablo Düzenleyici  ",
+    "kategori":["teknoloji",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+
+   
+{
+
+    "id": 112,
+    "ad":"Ekran Tmizleme Kiti   ",
+    "kategori":["teknoloji",],
+    "fiyat":300,
+    "fiyat_puanı" : 1
+},
+
+
+
+   
+{
+
+    "id": 113,
+    "ad":"RGB Masa Lambası  ",
+    "kategori":["teknoloji",],
+    "fiyat":1000,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+   
+{
+
+    "id": 114,
+    "ad":"Kablosuz Hızlı Şarj Standı   ",
+    "kategori":["teknoloji",],
+    "fiyat":1050,
+    "fiyat_puanı" : 2
+},
+
+
+
+   
+{
+
+    "id": 115,
+    "ad":"Grafi Tablet   ",
+    "kategori":["teknoloji",],
+    "fiyat":1200,
+    "fiyat_puanı" : 2
+},
+
+
+
+   
+{
+
+    "id": 116,
+    "ad":" Taşınabilir SSD  ",
+    "kategori":["teknoloji",],
+    "fiyat":1300,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+   
+{
+
+    "id": 117,
+    "ad":"Bluetooth Kulaklık  ",
+    "kategori":["teknoloji",],
+    "fiyat":2550,
+    "fiyat_puanı" : 3
+},
+
+
+
+   
+{
+
+    "id": 118,
+    "ad":"Mekanik Klavye  ",
+    "kategori":["teknoloji",],
+    "fiyat":2050,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+   
+{
+
+    "id": 119,
+    "ad":"Akıllı Saat   ",
+    "kategori":["teknoloji",],
+    "fiyat":2350,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+
+
+ # Teknoloji Kategorisi Bitiş
+
+
+
+#---------------------------------------------------------
+
+
+
+    #Ev Eşyaları Kategorisi Başlangıç
+
+
+
+{
+
+    "id": 120,
+    "ad":"Beton Saksı Seti   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+
+{
+
+    "id": 121,
+    "ad":" Led Işıklı Mandallı Fotoğraf Askısı   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+
+{
+
+    "id": 122,
+    "ad":"Dekoratif Tütsülük    ",
+    "kategori":["ev eşyaları",],
+    "fiyat":400,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+{
+
+    "id": 123,
+    "ad":"Kristal Küre Gece Lambası    ",
+    "kategori":["ev eşyaları",],
+    "fiyat":850,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+{
+
+    "id": 124,
+    "ad":"Geometrik Duvar Rafı   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":1050,
+    "fiyat_puanı" : 2
+},
+
+
+
+{
+
+    "id": 125,
+    "ad":"El Yapımı Duvar Süsü   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":1250,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+{
+
+    "id": 126,
+    "ad":"Akıllı RGB Şerit Led   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":1150,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+{
+
+    "id": 127,
+    "ad":" Havada Duran Saksı   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":3500,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+{
+
+    "id": 128,
+    "ad":"Büyük Boy Retro Dünya Haritası   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":2350,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+{
+
+    "id": 129,
+    "ad":"Pixel Art Akıllı Ekran   ",
+    "kategori":["ev eşyaları",],
+    "fiyat":2250,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+    #Ev Eşyaları Kategorisi Bitiş 
+
+
+#--------------------------------------------------------
+
+    #Oyuncu Ürünleri Kategorisi Başlangıç
+
+
+
+
+{
+
+    "id": 130,
+    "ad":"RGB MousePad   ",
+    "kategori":["oyuncu",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+
+{
+
+    "id": 131,
+    "ad":"Kulaklık Standı   ",
+    "kategori":["oyuncu",],
+    "fiyat":450,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+
+
+{
+
+    "id": 132,
+    "ad":"Temalı Işıklı Tabela   ",
+    "kategori":["oyuncu",],
+    "fiyat":400,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+
+
+
+{
+
+    "id": 133,
+    "ad":"Mouse ",
+    "kategori":["oyuncu",],
+    "fiyat":800,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+
+
+
+{
+
+    "id": 134,
+    "ad":"Mikrofon   ",
+    "kategori":["oyuncu",],
+    "fiyat":850,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+
+
+
+{
+
+    "id": 135,
+    "ad":"Mekanik Klavye   ",
+    "kategori":["oyuncu",],
+    "fiyat":1050,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+
+
+{
+
+    "id": 136,
+    "ad":"Akıllı Monitör Arkası Aydınlatma   ",
+    "kategori":["oyuncu",],
+    "fiyat":1250,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+
+
+{
+
+    "id": 137,
+    "ad":"Geri Bildirimli Kulaklık    ",
+    "kategori":["oyuncu",],
+    "fiyat":2250,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+
+
+{
+
+    "id": 138,
+    "ad":"Oyuncu Koltuğu   ",
+    "kategori":["oyuncu",],
+    "fiyat":3250,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+
+
+{
+
+    "id": 139,
+    "ad":"El Konsolu  ",
+    "kategori":["oyuncu",],
+    "fiyat":2550,
+    "fiyat_puanı" : 3
+},
+
+
+    #Oyuncu Ürünleri Kategorisi Bitti
+
+
+#----------------------------------------------------------
+
+
+    #Hobi Eşyaları Kategorisi Başlangıç
+
+{
+
+    "id": 140,
+    "ad":"Boyama Seti  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":250,
+    "fiyat_puanı" : 1
+},
+
+
+
+{
+
+    "id": 141,
+    "ad":"Nakış SETİ  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+
+{
+
+    "id": 142,
+    "ad":"Kristal Oluşturma Kiti  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":450,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+{
+
+    "id": 143,
+    "ad":"Parfüm Kiti  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":850,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+{
+
+    "id": 144,
+    "ad":"Lego Oyuncak  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":950,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+{
+
+    "id": 145,
+    "ad":" Kendi Saatini Yap  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":1050,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+
+{
+
+    "id": 146,
+    "ad":" Bira Yapma Kiti  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":1250,
+    "fiyat_puanı" : 2
+},
+
+
+
+
+
+{
+
+    "id": 147,
+    "ad":" Teleskop  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":2250,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+
+{
+
+    "id": 148,
+    "ad":"El Yapımı Oyuncak Gemi    ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":2550,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+{
+
+    "id": 149,
+    "ad":"Fotoğraf Makinesi  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":4250,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+{
+
+    "id": 150,
+    "ad":"Gimball  ",
+    "kategori":["hobi eşyaları",],
+    "fiyat":4250,
+    "fiyat_puanı" : 3
+},
+
+
+
+
+#Hobi Eşyaları Kategorisi Bitiş 
+
+
+
+#-----------------------------------------------
+
+
+
+
+
+
+
+
+
+]
