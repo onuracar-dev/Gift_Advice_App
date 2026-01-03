a91063c1553ef50d11b9e31671b89771677e614c
Ömer Kazak <omerkazak1441@gmail.com>
2026-01-03 04:56:39 +0300

    iki kategori oluşturuldu

--- data.py
+++ data.py
@@ -1,13 +1,241 @@
 hediye_veritabanı=[
-    
+
+
+     # Mutfak Kategorisi Başlangıcı   
+
+
 {
 
     "id": 101,
     "ad":"Dijital Mutfak Tartısı",
-    "kategori":["Mutfak",],
+    "kategori":["mutfak",],
     "fiyat":300,
     "fiyat_puanı" : 1
-}
+},
+
+    
+{
+
+    "id": 102,
+    "ad":"Kesme Tahtası Seti",
+    "kategori":["mutfak",],
+    "fiyat":400,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 103,
+    "ad":"İsimli Mutfak Önlüğü",
+    "kategori":["mutfak",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 104,
+    "ad":"Vakumlu Saklama Kabı Seti",
+    "kategori":["mutfak",],
+    "fiyat":1000,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 105,
+    "ad":"Döküm Izgara Tava",
+    "kategori":["mutfak",],
+    "fiyat":1200,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 106,
+    "ad":"Kahve Başlangıç Seti",
+    "kategori":["mutfak",],
+    "fiyat":1400,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 107,
+    "ad":"Airfryer",
+    "kategori":["mutfak",],
+    "fiyat":3000,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 108,
+    "ad":"Kahve Makinesi",
+    "kategori":["mutfak",],
+    "fiyat":4000,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 109,
+    "ad":"Mutfak Şefi Makinesi",
+    "kategori":["mutfak",],
+    "fiyat":4000,
+    "fiyat_puanı" : 1
+},
+
+
+    #Mutfak Kategorisi Burada Bitti
+
+
+
+
+
+
+    #Teknoloji Kategorisi Başlangıcı
+{
+
+    "id": 110,
+    "ad":"Airtag ",
+    "kategori":["teknoloji",],
+    "fiyat":400,
+    "fiyat_puanı" : 1
+},
+
+
+    
+{
+
+    "id": 111,
+    "ad":"Kablo Düzenleyici  ",
+    "kategori":["teknoloji",],
+    "fiyat":350,
+    "fiyat_puanı" : 1
+},
+
+
+
+   
+{
+
+    "id": 112,
+    "ad":"Ekran Tmizleme Kiti   ",
+    "kategori":["teknoloji",],
+    "fiyat":300,
+    "fiyat_puanı" : 1
+},
+
+
+
+   
+{
+
+    "id": 113,
+    "ad":"RGB Masa Lambası  ",
+    "kategori":["teknoloji",],
+    "fiyat":1000,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+   
+{
+
+    "id": 114,
+    "ad":"Kablosuz Hızlı Şarj Standı   ",
+    "kategori":["teknoloji",],
+    "fiyat":1050,
+    "fiyat_puanı" : 1
+},
+
+
+
+   
+{
+
+    "id": 115,
+    "ad":"Grafi Tablet   ",
+    "kategori":["teknoloji",],
+    "fiyat":1200,
+    "fiyat_puanı" : 1
+},
+
+
+
+   
+{
+
+    "id": 116,
+    "ad":" Taşınabilir SSD  ",
+    "kategori":["teknoloji",],
+    "fiyat":1300,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+   
+{
+
+    "id": 117,
+    "ad":"Bluetooth Kulaklık  ",
+    "kategori":["teknoloji",],
+    "fiyat":2550,
+    "fiyat_puanı" : 1
+},
+
+
+
+   
+{
+
+    "id": 118,
+    "ad":"Mekanik Klavye  ",
+    "kategori":["teknoloji",],
+    "fiyat":2050,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+   
+{
+
+    "id": 119,
+    "ad":"Akıllı Saat   ",
+    "kategori":["teknoloji",],
+    "fiyat":2350,
+    "fiyat_puanı" : 1
+},
+
+
+
+
+
+
+ # Teknoloji Kategorisi Bitiş
+
+
 
 
 
